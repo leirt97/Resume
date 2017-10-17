885f548df1e0263d009a6eb2c88f3255a91c653e
leirt97 <leirt97@gmail.com>
2017-10-17 23:47:21 +0800

    添加个人信息页

--- js/index.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.container').on('click', (e) => {
-        let html = $(e.target).attr('data-html')
-        window.location.href = './html/'+html+'.html'
-    })
-})
--- readme.md
+++
@@ -1,2 +0,0 @@
-# 个人简历   
-[预览](https://leirt97.github.io/Resume/index.html)
